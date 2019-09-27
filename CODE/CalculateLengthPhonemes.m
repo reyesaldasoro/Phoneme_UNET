c99cefa3ef422acd213b9dd5f98e77213f13fb21
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2019-09-27 11:13:12 +0100

    Update CalculateLengthPhonemes.m
    add results of lenght of phonemes

--- CODE/CalculateLengthPhonemes.m
+++ CODE/CalculateLengthPhonemes.m
@@ -22,29 +22,36 @@ else
 end
 %% Read the folder for .lab and . wav files
 
-    % all files in a folder will be converted
-    dir_Phonemes                    = dir(strcat(dataSetDir,'/*.lab'));
-    dir_Sounds                      = dir(strcat(dataSetDir,'/*.wav'));
-    
-    numFiles                        = size(dir_Phonemes,1);
+% all files in a folder will be converted
+dir_Phonemes                    = dir(strcat(dataSetDir,'/*.lab'));
+dir_Sounds                      = dir(strcat(dataSetDir,'/*.wav'));
+
+numFiles                        = size(dir_Phonemes,1);
 %% To prepare data read per case, train 50% test 50%
-    
+
 %
 clear LengthPhonemes
 LengthPhonemes(numFiles*10,1) = 0;
 t=1;
 for counterFile = 1:numFiles
+    
+    disp(counterFile)
+    % Read the *.lab files, these are text with start-end-phoneme
+    LAB                             = importdata(strcat(dataSetDir,'/',dir_Phonemes(counterFile).name,' '));
+    % convert to a matlab matrix for easier manipulation
+    [Phonemes,numPhonemes]          = convert_LAB_to_Phonemes(LAB);
+    [audioWave,sampleRate]          = audioread(strcat(dataSetDir,'/',dir_Sounds(counterFile).name));
+    % Size of phonemes
+    % discard 3 at the beginning, usually sil-breath-sil and last -sil
+    for k=4:numPhonemes-1
+        LengthPhonemes(t,1) = sampleRate*(Phonemes{k,2}-Phonemes{k,1})/1000;
+        t=t+1;
+    end
+end
 
-disp(counterFile)
-% Read the *.lab files, these are text with start-end-phoneme
-LAB                             = importdata(strcat(dataSetDir,'/',dir_Phonemes(counterFile).name,' '));
-% convert to a matlab matrix for easier manipulation
-[Phonemes,numPhonemes]          = convert_LAB_to_Phonemes(LAB);
-[audioWave,sampleRate]          = audioread(strcat(dataSetDir,'/',dir_Sounds(counterFile).name));       
-% Size of phonemes
-% discard 3 at the beginning, usually sil-breath-sil and last -sil
-for k=4:numPhonemes-1
-LengthPhonemes(t,1) = sampleRate*(Phonemes{k,2}-Phonemes{k,1})/1000;
-t=t+1;
-end
-end
+%%
+% Results (samples x 1000): 
+% Shortest Phoneme      =  0.48
+% Longest Phoneme       = 20.48
+% Average Length        =  1.4189
+% 95% are shorter than  =  2.9
